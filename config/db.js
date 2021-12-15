5805f8a438d9b7c4ec3891d8663b18c738b99c61
Mudassar755 <mudasar.se@gmail.com>
2021-12-16 01:20:35 +0500

    update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
-const config = require('config')
-const db = config.get('mongoURL')
 const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB =async()=>{
-    // console.log("db=====>>", db)
     try{
         await mongoose.connect(process.env.mongoURL);
         console.log('MongoDb Connected...')
